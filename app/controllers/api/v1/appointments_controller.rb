d4e2a694cd26cf9e3255e2df1ee7c00a0baee23b
Paul Rosero <paul_rosero@yahoo.com>
2020-03-09 19:40:17 -0400

    add temporary code to have a restful Appointment CRUD controller.

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::AppointmentsController < ApplicationController
+    before_action :find_appointment, only: [:show, :edit, :update, :destroy]
+
+    def index 
+        @appointments = Appoointment.all
+        render json: @appointments
+    end
+
+    def show
+        render json: @appointment
+    end
+
+    def create 
+        @appointment = Appoointment.new(appointment_params)
+        save_or_update_appointment
+    end
+
+    def edit
+        render json: @appointment
+    end
+
+    def update
+        @appointment.update(appointment_params)
+        save_or_update_appointment
+    end
+
+    def destroy
+        @appointment.delete
+        render json: {appointmentId: @appointment.id}
+    end
+
+    private 
+    
+    def appointment_params
+        params.require(:appointment).permit(:massage_therapist, :client, :modality, :appointment_time, :special_request)
+    end
+
+    def find_appointment
+        @appointment = Appoointment.find_by_id(params[:id])
+    end
+
+    def save_or_update_appointment
+        if @appointment.save
+            render json: @appointment 
+        else
+            render json: {errors: @appointment.errors.full_messages}
+        end
+    end
 end
