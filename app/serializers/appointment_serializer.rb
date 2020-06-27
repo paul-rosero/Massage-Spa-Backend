eb7726d680bb0fa0c62b21f5931429719be6b84a
Paul Rosero <paul_rosero@yahoo.com>
2020-06-27 11:54:29 -0400

    change attribute from appointment_time to date_and_time to be more accurate

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,5 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :modality, :appointment_time, :special_request
+  attributes :id, :modality, :date_and_time, :special_request
   belongs_to :massage_therapist
   belongs_to :client
 
